651e3cc654b96a2eb397f284a0f51a6c21cff461
mabellemos <mils2@aluno.ifal.edu.br>
2023-12-08 21:16:57 +0000

    finalizacoes

--- Main.py
+++ Main.py
@@ -1,101 +1,7 @@
-# #Importação das classes
-# from Usuario import Usuario
-# from Vendas import Vendas
-# from Produto import Produto
-# from Funcionario import Funcionario
-
-# #Arrays
-# produtos = []
-# funcionarios = []
-# vendas = []
-
-# #Instanciamento dos objetos
-# objProdutos = Produtos()
-# objVendas = Vendas()
-# objFuncionarios = Funcionarios()
-
-# #Entrada no sistema por autenticação
-# print('\n\nPANIFICADORA\n')
-
-# user = str(input('Usuário: '))
-# senha = str(input('Senha: '))
-
-# usuario = Usuario(user, senha)
-# verificado = usuario.logar(user, senha)
-
-# if(verificado == True):
-#     print('\nBem-vindo ao Sistema\n')
-#     while True:
-#         print('\nPANIFICADORA\n')
-#         print('\nSelecione a opção desejada:\n\n1 - Produtos\n2 - Funcionários\n3 - Vendas\n')
-#         opcaoMenuPrinc = int(input())
-        
-#         if(opcaoMenuPrinc == 1):
-#             print('\nGestão de Produtos\n')
-#             print('\n1 - Cadastrar Novo Produto\n2 - Alterar Dados do Produto\n3 - Visualizar Estoque\n4 - Sair')
-#             opcaoMenuProd = int(input())
-
-#             if(opcaoMenuProd  == 1):
-#                 numP = int(input("\nInforme quantos produtos deseja cadastrar: "))
-
-#                 for i in range(numP):
-#                     produtos.append(objProdutos.cadastrarProdutos())
-
-#             elif(opcaoMenuProd == 2):
-#                 idP = int(input("\nInforme o ID do produto que deseja alterar: "))
-#                 produtos.append(objProdutos.alterarProduto(id))
-#             elif(opcaoMenuProd  == 3):
-#                 produtos.append(objProdutos.mostrarProdutos())
-#             elif(opcaoMenuProd  == 4):
-#                 break
-#             else:
-#                 print('\nOpção inválida')
-
-#         if(opcaoMenuPrinc == 2):
-#             print('\nGestão de Funcionários\n')
-#             print('\n1 - Cadastrar Novo Funcionário\n2 - Alterar Dados do Funcionário\n3 - Visualizar Funcionários')
-#             opcaoMenuFunc = int(input())
-
-#             if(opcaoMenuFunc == 1):
-#                 quantF = int(input("\nInforme a quantidade de funcionários que deseja cadastrar: "))
-
-#                 for i in range(quantF):
-#                     funcionarios.append(objFuncionarios.salvar())
-#             elif(opcaoMenuFunc == 2):
-#                 funcionarios.append(objFuncionariosalterarFunc())
-#             elif(opcaoMenuFunc == 3):
-#                 funcionarios.append(objFuncionarios.exibir())
-#             else:
-#                 print('\nOpção inválida')
-
-#         if(opcaoMenuPrinc == 3):
-#             print('\nGestão de Vendas\n')
-#             print('\n1 - Caixa\n2 - Atualizar Estoque\n3 - Visualizar Estoque')
-#             opcaoMenuVend = int(input())
-
-#             if(opcaoMenuVend == 1):
-#                 vendas.append(objVendas.vender())
-#             elif( opcaoMenuVend == 2):
-#                 vendas.append(objVendas.atualizarEstoque())
-#             elif(opcaoMenuVend == 2):
-#                 produtos.append(objProdutos.mostrarProdutos())
-#             else:
-#                 print('\nOpção inválida')
-
-#         resp = str(input('\nDeseja realizar outra operação? (sim/não)'))
-#         if (resp.lower() == "nao" or resp.lower() == "não"):
-#             print('\nSistema fechado!\n')
-#             break
-# else:
-#     print('\nUsuário e/ou senha inválidos!')
-
-#Entrada no sistema por autenticação
-
-# Imports
-
 from functions.Funcionario import Funcionario
 from functions.Usuario import Usuario
 from functions.Produto import Produto
+from functions.Vendas import Vendas
 
 # Menus 
 
@@ -107,20 +13,20 @@ def MenuPrincipal():
     elif(opcaoMenuPrinc == 2):
         MenuDeFuncionario()
     elif(opcaoMenuPrinc == 3):
-        print("\nMenu de Vendas: ")
+        menuVendas()
 
 def MenuDeProduto():
 
     condicao = True
 
-    print("\nMenu de Produtos")
-    print('\n1 - Cadastrar Novo Produto\n2 - Alterar Dados do Produto\n3 - Visualizar Estoque\n4 - Sair')
+    print("\n--------Menu de Produtos-------\n")
+    print('\n1 - Cadastrar Novo Produto\n2 - Alterar Dados do Produto\n3 - Visualizar Estoque\n4 - Sair\n')
     opcaoMenuProd = int(input())
 
     if(opcaoMenuProd == 1):
         while condicao:
 
-            print ("Adicione os parâmetros do produto\n")
+            print ("\nAdicione os parâmetros do produto\n")
             nome = input("Adicione o nome:")
             valor = float(input("\nAdicione o preço:"))
             unidade = int(input("\nAdicione as unidades:"))
@@ -156,8 +62,7 @@ def MenuDeProduto():
     elif(opcaoMenuProd == 4):
         MenuPrincipal()
 
-def MenuDeFuncionario():    
-    # condicao = True
+def MenuDeFuncionario():
 
     print("\nMenu de Funcionários")
     print('\n1 - Cadastrar Novo Funcionário\n2 - Alterar Dados do Funcionário\n3 - Visualizar Funcionários\n4 - Sair')
@@ -180,6 +85,23 @@ def MenuDeFuncionario():
     elif(opcaoMenuFunc == 4):
         MenuPrincipal()
 
+def menuVendas():
+    while True:
+
+        opc = int(input("\n------- MENU VENDAS ------\n\n1 - Caixa\n2 - Atualizar Estoque\n"))
+
+        if (opc == 1):
+            Vendas.vender()
+        elif (opc == 2):
+            id = input("\nInforme o ID do produto: ")
+            quant = int(input("\nInforme a nova quantidade do produto: "))
+            Vendas.atualizarEstoque(id, quant)
+        else:
+            print("\nOpção inválida!\n")
+
+        resp = str(input("\nDeseja realizar outra opção? (sim/não)\n"))
+        if (resp.lower() == 'nao'):
+            break
 # Código
 
 print('\n\n--------PANIFICADORA--------\n')
